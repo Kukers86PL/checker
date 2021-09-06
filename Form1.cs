c646c5ea822330077f224eb3c129285ed4010636
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2021-09-06 04:09:31 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -157,7 +157,7 @@ namespace checker
             }
 
             UdpClient udpClient = new UdpClient(IP_ADDRESS, PORT);
-            Byte[] sendBytes = Encoding.UTF8.GetBytes(message);
+            Byte[] sendBytes = Encoding.UTF8.GetBytes(Convert.ToBase64String(Encoding.UTF8.GetBytes(message)));
             try
             {
                 udpClient.Send(sendBytes, sendBytes.Length);
